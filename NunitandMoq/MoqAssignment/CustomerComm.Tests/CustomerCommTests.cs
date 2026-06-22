2901438eb73cd95b1730ac01ae900401ea2d6c66
Shreya <shreyaoff25@gmail.com>
2026-06-22 23:09:30 +0530

    Completed Moq handson task 1 and task 2

---
+++ NunitandMoq/MoqAssignment/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,33 @@
+using Moq;
+using NUnit.Framework;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mockMailSender;
+        private CustomerCommLib.CustomerComm _customerComm;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+
+            _mockMailSender
+                .Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            _customerComm = new CustomerCommLib.CustomerComm(_mockMailSender.Object);
+        }
+
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue()
+        {
+            bool result = _customerComm.SendMailToCustomer();
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
