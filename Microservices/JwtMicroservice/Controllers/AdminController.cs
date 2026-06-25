481f93b1198082d95b1440c486d3da05600d845c
Shreya <shreyaoff25@gmail.com>
2026-06-26 01:19:20 +0530

    Complete Microservices JWT Authentication handson

---
+++ Microservices/JwtMicroservice/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtMicroservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet("dashboard")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetDashboard()
+        {
+            return Ok("Welcome Admin!");
+        }
+    }
+}
